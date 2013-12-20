558102a4bd000d6b6e95b991f1045af710abd5f4
Dennis Gokman <dennis.gokman@gmail.com>
2013-12-19 23:28:04 -0500

    story editing

--- views/v_index_index.php
+++ views/v_index_index.php
@@ -1,7 +1,7 @@
 <link rel="stylesheet" type="text/css" href="css/sample-app.css"/>
    <div id="welcome">
       <h1><?=APP_NAME?></h1><br>
-          <a href="/posts/add">Start chatting</a><br>
+          <a href="/posts/add">Create your story!</a><br>
           
    </div>
      
--- views/v_posts_index.php
+++ views/v_posts_index.php
@@ -1,5 +1,5 @@
 <?php foreach($posts as $post): ?>
 <h1>DESCRIPTION OF THE LOVELY GROUP THAT I AM IN</h1> 
-   <p>We are having a perfectly <?=$post['adj1']?> time this evening in the <?=$post['adj2']?> home of <?=$post['name1']?>. The rooms are decorated <?=$post['adverb1']?> with many stylish <?=$post['plnoun1']?>
+   <p>We are having a perfectly <b><?=$post['adj1']?></b> time this evening in the <b><?=$post['adj2']?></b> home of <b><?=$post['name1']?></b>. The rooms are decorated <b><?=$post['adverb1']?></b> with many stylish <b><?=$post['plnoun1']?></b> that must have cost at least <b><?=$post['number1']?></b> dollars. The guests are all <b><?=$post['adj3']?></b> conversationalists and are all <b><?=$post['adverb2']?></b> dressed. <b><?=$post['name2']?></b> has been entertaining us by telling about the time he showed his <b><?=$post['adj4']?></b> <b><?=$post['noun1']?></b> to <b><?=$post['name3']?></b> who mistook it for an early American <b><?=$post['noun2']?></b>. The refreshments are from <b><?=$post['brand']?></b> and the idea of serving <b><?=$post['liquid']?></b> on the rocks showed <b><?=$post['adj5']?></b> imagination. Visiting here is always a (an) <b><?=$post['adj6']?></b> experience
 </p>
 <?php endforeach; ?>
